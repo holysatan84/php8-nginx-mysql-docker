e8973110109d32a24f49d24a603975184e7109fe
Sudeep Deepak Padalkar <holysatan84@gmail.com>
2022-10-12 00:59:34 +0200

    Anonymouse classes exmaple

--- src/public/index.php
+++ src/public/index.php
@@ -5,5 +5,11 @@ $autoloadFile = __DIR__ . "/../vendor/autoload.php";
 if(!file_exists($autoloadFile))  exit("run composer install");
 require_once __DIR__ . "/../vendor/autoload.php";
 
+$anonClassObj = new class(1, 2, 3) extends App\SomeClass implements App\SomeInterface {
+    use App\SomeTrait;
+    public function __construct(public int $x, public int $y, public int $z){}
+};
 
-phpinfo();
+var_dump($anonClassObj);
+
+var_dump(get_class($anonClassObj));
