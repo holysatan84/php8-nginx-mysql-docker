b0faa27b4fd2b2e37ffe1bdd5476cf256db6d7b8
Sudeep Deepak Padalkar <holysatan84@gmail.com>
2022-10-13 14:25:23 +0200

    added response code examples

--- src/public/index.php
+++ src/public/index.php
@@ -9,12 +9,20 @@ session_start();
 
 define('VIEW_PATH', __DIR__ . '/../app/views/');
 
-$router = new App\Router();
+try {
 
-$router
-    ->get('/', [App\Controllers\HomeController::class, 'index'])
-    ->get('/invoices', [App\Controllers\InvoiceController::class, 'index'])
-    ->get('/invoices/create', [App\Controllers\InvoiceController::class, 'create'])
-    ->post('/invoices/create', [App\Controllers\InvoiceController::class, 'store']);
 
-echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+    $router = new App\Router();
+
+    $router
+        ->get('/', [App\Controllers\HomeController::class, 'index'])
+        ->get('/invoices', [App\Controllers\InvoiceController::class, 'index'])
+        ->get('/invoices/create', [App\Controllers\InvoiceController::class, 'create'])
+        ->post('/invoices/create', [App\Controllers\InvoiceController::class, 'store']);
+
+    echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+} catch (\App\Exception\RouteNotFoundException $e) {
+    http_response_code(404);
+
+    echo \App\View::make('error/404');
+}
