54c286208f73d95b552e24dee7add2d23c4c890d
Sudeep Deepak Padalkar <holysatan84@gmail.com>
2022-10-13 17:42:43 +0200

    setup init mysql scripts

--- _docker/_init/initdb.sql
+++ _docker/_init/initdb.sql
@@ -1,2 +1,24 @@
-CREATE DATABASE IF NOT EXISTS `mydb`;
-GRANT ALL ON `mydb`.* TO 'db_admin'@'%';
+CREATE DATABASE IF NOT EXISTS `my-db`;
+GRANT ALL ON `my-db`.* TO 'db_admin'@'%';
+
+use `my-db`;
+
+create table users
+(
+    id         int unsigned primary key auto_increment,
+    email      varchar(225)      NOT NULL,
+    full_name  varchar(255)      NOT NULL,
+    is_active  boolean DEFAULT 0 NOT NULL,
+    created_at datetime          NOT NULL,
+    KEY is_active (is_active),
+    UNIQUE KEY email (email)
+);
+
+
+create table invoices
+(
+    id      int unsigned PRIMARY KEY AUTO_INCREMENT,
+    amount  decimal(10, 4),
+    user_id int unsigned,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL ON UPDATE CASCADE
+);
