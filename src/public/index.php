579bc4a56ea33d3beff4d96849c98ccfe09e4e02
Sudeep Deepak Padalkar <holysatan84@gmail.com>
2022-10-13 19:13:21 +0200

    added dotenv support

--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,14 @@
 <?php 
 declare(strict_types = 1);
 
+session_start();
+
 $autoloadFile = __DIR__ . "/../vendor/autoload.php";
 if(!file_exists($autoloadFile))  exit("run composer install");
 require_once __DIR__ . "/../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 
 phpinfo();
